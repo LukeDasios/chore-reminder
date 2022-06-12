34bfcb4bdd90dac84fa7742935c7450f0f239b51
LukeDasios <luke.dasios321@gmail.com>
2022-06-11 20:17:21 -0400

    Sent my first SMS message

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-const { express } = require("express")
-const { axios } = require("axios")
+const twilio = require("twilio")
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN // Your Auth Token from www.twilio.com/console
+
+const client = new twilio(accountSid, authToken)
+
+client.messages
+  .create({
+    body: "Hello from Node!",
+    to: process.env.MY_PHONE_NUMBER, // Text this number
+    from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid))
