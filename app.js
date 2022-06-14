ad0b0b78e7fa74bfcd6773a2cb12f388016cc27a
LukeDasios <luke.dasios321@gmail.com>
2022-06-13 20:04:49 -0400

    Ready

--- app.js
+++ app.js
@@ -3,60 +3,84 @@ const accountSid = process.env.ACCOUNT_SID
 const authToken = process.env.AUTH_TOKEN
 const TWILIO_PHONE_NUMBER = process.env.PHONE_NUMBER
 
-console.log("*******************")
-console.log(accountSid, authToken, TWILIO_PHONE_NUMBER)
-console.log("*******************")
-
 const client = require("twilio")(accountSid, authToken)
+
+let garbageWeek = true
+const theBoys = ["Luke", "Duncan", "Sam", "Jp"]
+const numbers = ["+16479385063", "+14168261333", "+14168447692", "+14166169331"]
+let iter = 3
 
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-  sendFirstMessage()
+  client.messages.create({
+    body: "App is live!",
+    from: TWILIO_PHONE_NUMBER,
+    to: "+16479385063",
+  })
+  setTimeout(tuesdayMessage, 85980000)
 })
 
 app.get("/", (req, res) => {
   res.send("Plz work!")
 })
 
-const sendFirstMessage = () => {
+// Tuesday @8pm
+const tuesdayMessage = () => {
   client.messages
     .create({
-      body: "First Message!",
+      body: garbageWeek
+        ? `Good Evening ${theBoys[iter]}! In case you haven't done so already, the Recycling, Compost, and Garbage need to be taken to the curb by tonight. Cheers.`
+        : `Good Evening ${theBoys[iter]}! In case you haven't done so already, the Recycling and Compost need to be taken to the curb by tonight. Cheers.`,
       from: TWILIO_PHONE_NUMBER,
-      to: "+16479385063",
+      to: numbers[iter],
     })
     .then((message) => {
-      console.log(message.body)
-      setTimeout(sendSecondMessage, 5000)
+      garbage = !garbage
+      setTimeout(sundayMessage1, 396000000)
     })
 }
 
-const sendSecondMessage = () => {
+// Sunday @10am
+const sundayMessage1 = () => {
   client.messages
     .create({
-      body: "Second Message!",
+      body: `Good Morning ${
+        theBoys[iter]
+      }! Please empty the Recycling, Green bin, and Garbage one last time so that ${whoIsNext()} may start their week with a clean slate. After that, you are free!`,
       from: TWILIO_PHONE_NUMBER,
-      to: "+16479385063",
+      to: numbers[iter],
     })
     .then((message) => {
-      console.log(message.body)
-      setTimeout(sendFirstMessage, 5000)
+      if (iter === 3) {
+        iter = 0
+      } else {
+        iter++
+      }
+      setTimeout(sundayMessage2, 28800000)
     })
 }
 
-// const sendThirdMessage = () => {
-//   client.messages
-//     .create({
-//       body: "Third Message!",
-//       from: TWILIO_PHONE_NUMBER,
-//       to: "+16479385063",
-//     })
-//     .then((message) => {
-//       console.log(message.body)
-//       setTimeout(sendFirstMessage, 5000)
-//     })
-// }
+// Sunday @6pm
+const sundayMessage2 = () => {
+  client.messages
+    .create({
+      body: `Good Evening ${theBoys[iter]}! Heads up, You're on garbage this week.`,
+      from: TWILIO_PHONE_NUMBER,
+      to: numbers[iter],
+    })
+    .then((message) => {
+      console.log(message.body)
+      setTimeout(tuesdayMessage, 180000000)
+    })
+}
+
+function whoIsNext(num) {
+  if (num === 3) {
+    return "Luke"
+  } else {
+    return theBoys[num + 1]
+  }
+}
--- message-sender.js
+++ message-sender.js
@@ -1,93 +1,78 @@
-// require("dotenv").config()
+require("dotenv").config()
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID
-// const authToken = process.env.TWILIO_AUTH_TOKEN
-// const messagingServiceSid = process.env.MESSAGING_SERVICE_SID
-// const client = require("twilio")(accountSid, authToken)
-// const ONE_WEEK = 604800000
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const messagingServiceSid = process.env.MESSAGING_SERVICE_SID
+const client = require("twilio")(accountSid, authToken)
+const ONE_WEEK = 604800000
 
-// let garbageWeek = true
-// const theBoys = ["Luke", "Duncan", "Sam", "Jp"]
-// const numbers = ["+16479385063", "+14168261333", "+14168447692", "+14166169331"]
-// let iter = 3
+let garbageWeek = true
+const theBoys = ["Luke", "Duncan", "Sam", "Jp"]
+const numbers = ["+16479385063", "+14168261333", "+14168447692", "+14166169331"]
+let iter = 3
 
-// let date1 = new Date(Date.UTC(2022, 6, 14, 17, 20))
-// let date2 = new Date(Date.UTC(2022, 6, 19, 10, 0))
 
-// const daysInMonth = new Map()
-// daysInMonth.set(1, 31)
-// daysInMonth.set(2, 28)
-// daysInMonth.set(3, 31)
-// daysInMonth.set(4, 30)
-// daysInMonth.set(5, 31)
-// daysInMonth.set(6, 30)
-// daysInMonth.set(7, 31)
-// daysInMonth.set(8, 31)
-// daysInMonth.set(9, 30)
-// daysInMonth.set(10, 31)
-// daysInMonth.set(11, 30)
-// daysInMonth.set(12, 31)
 
-// // Sent on Sunday Morning @10am
-// const sundayMessages = (date) => {
-//   client.messages
-//     .create({
-//       messagingServiceSid: messagingServiceSid,
-//       body: garbageWeek
-//         ? `Good Evening ${theBoys[iter]}! In case you haven't already done so already, friendly reminder that the Recycling, Compost, and Garbage need to be taken to the curb by tonight. Cheers.`
-//         : `Good Evening ${theBoys[iter]}! In case you haven't already done so already, friendly reminder that the Recycling and Compost need to be taken to the curb by tonight. Cheers.`,
-//       sendAt: date,
-//       scheduleType: "fixed",
-//       statusCallback: null,
-//       to: "+16479385063", //numbers[iter]
-//     })
-//     .then((message) => {
-//       date1 = calculateNextDate()
-//       setTimeout(sundayMessages, ONE_WEEK, date1)
-//     })
-// }
+// Sent on Sunday Morning @10am
+const sundayMessages = (date) => {
+  client.messages
+    .create({
+      messagingServiceSid: messagingServiceSid,
+      body: garbageWeek
+        ? `Good Evening ${theBoys[iter]}! In case you haven't already done so already, friendly reminder that the Recycling, Compost, and Garbage need to be taken to the curb by tonight. Cheers.`
+        : `Good Evening ${theBoys[iter]}! In case you haven't already done so already, friendly reminder that the Recycling and Compost need to be taken to the curb by tonight. Cheers.`,
+      sendAt: date,
+      scheduleType: "fixed",
+      statusCallback: null,
+      to: "+16479385063", //numbers[iter]
+    })
+    .then((message) => {
+      date1 = calculateNextDate()
+      setTimeout(sundayMessages, ONE_WEEK, date1)
+    })
+}
 
-// // Sent on Tuesday Night @8pm
-// const tuesdayMessage = (date) => {
-//   client.messages
-//     .create({
-//       messagingServiceSid: messagingServiceSid,
-//       body: "This is a scheduled message",
-//       sendAt: date,
-//       scheduleType: "fixed",
-//       statusCallback: "https://abc1234.free.beeceptor.com",
-//       to: "+16479385063", //numbers[iter]
-//     })
-//     .then((message) => {
-//       if (iter + 1 === theBoys.length) {
-//         iter = 0
-//       } else {
-//         iter++
-//       }
-//       garbageWeek = !garbageWeek
-//       date = calculateNextDate()
-//       setTimeout(tuesdayMessage, ONE_WEEK, date)
-//     })
-// }
+// Sent on Tuesday Night @8pm
+const tuesdayMessage = (date) => {
+  client.messages
+    .create({
+      messagingServiceSid: messagingServiceSid,
+      body: "This is a scheduled message",
+      sendAt: date,
+      scheduleType: "fixed",
+      statusCallback: "https://abc1234.free.beeceptor.com",
+      to: "+16479385063", //numbers[iter]
+    })
+    .then((message) => {
+      if (iter + 1 === theBoys.length) {
+        iter = 0
+      } else {
+        iter++
+      }
+      garbageWeek = !garbageWeek
+      date = calculateNextDate()
+      setTimeout(tuesdayMessage, ONE_WEEK, date)
+    })
+}
 
-// function calculateNextDate() {
-//   let date = new Date()
-//   let year = date.getFullYear()
-//   let month = date.getMonth()
-//   let day = date.getDate()
-//   let hour = date.getHours()
+function calculateNextDate() {
+  let date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth()
+  let day = date.getDate()
+  let hour = date.getHours()
 
-//   if (day + 7 > daysInMonth[month]) {
-//     day = day + 7 - daysInMonth[month]
-//     if (month === 12) {
-//       month = 0
-//     }
-//   } else {
-//     day += 7
-//   }
+  if (day + 7 > daysInMonth[month]) {
+    day = day + 7 - daysInMonth[month]
+    if (month === 12) {
+      month = 0
+    }
+  } else {
+    day += 7
+  }
 
-//   return new Date(Date.UTC(year, month, day, hour))
-// }
+  return new Date(Date.UTC(year, month, day, hour))
+}
 
-// tuesdayMessage(date1)
-// sundayMessages(date2)
+tuesdayMessage(date1)
+sundayMessages(date2)
